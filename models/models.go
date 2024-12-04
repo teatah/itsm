929591b491c15f742566ffab95d803e6d93c9f20
Dyglaz <kallkae@mail.ru>
2024-12-05 02:08:35 +0300

    add a module with business services

--- models/models.go
+++ models/models.go
@@ -5,16 +5,20 @@ type User struct {
 	Username string `gorm:"not null"`
 	Password string `gorm:"not null"`
 	IsAdmin  bool   `gorm:"default:false"`
+	IsTech   bool   `gorm:"default:false"`
 }
 
-type BusinessService struct {
+type ServiceLine struct {
 	ID   uint   `gorm:"primaryKey"`
 	Name string `gorm:"not null"`
 }
 
-type TechnicalService struct {
-	ID   uint   `gorm:"primaryKey"`
-	Name string `gorm:"not null"`
+type Service struct {
+	ID            uint        `gorm:"primaryKey"`
+	ServiceLineID uint        `gorm:"not null"`
+	Name          string      `gorm:"not null"`
+	Description   string      `json:"description"`
+	ServiceLine   ServiceLine `gorm:"foreignKey:ServiceLineID"`
 }
 
 type Incident struct {
