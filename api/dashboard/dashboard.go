f46b719d34c3a4c5f3316edd429e7ac3277f4f25
Dyglaz <kallkae@mail.ru>
2024-12-07 04:08:45 +0300

    Add technical services and update business services

--- api/dashboard/dashboard.go
+++ api/dashboard/dashboard.go
@@ -6,11 +6,8 @@ import (
 	"gorm.io/gorm"
 	"html/template"
 	"itsm/models"
-	"itsm/session"
-	"itsm/utils"
-	"net/http"
-
 	_ "itsm/session"
+	"net/http"
 )
 
 var db *gorm.DB
@@ -18,8 +15,6 @@ var db *gorm.DB
 func SetupRoutes(r *mux.Router, database *gorm.DB) {
 	db = database
 	r.HandleFunc("/dashboard", dashboardHandler)
-	r.HandleFunc("/business-services", businessServicesHandler)
-	r.HandleFunc("/technical-services", technicalServicesHandler)
 	r.HandleFunc("/incidents", incidentsHandler)
 	r.HandleFunc("/messenger", messengerHandler)
 	r.HandleFunc("/users/get", getUsersHandler)
@@ -77,42 +72,6 @@ func dashboardHandler(w http.ResponseWriter, _ *http.Request) {
 	if err != nil {
 		return
 	}
-}
-
-func businessServicesHandler(w http.ResponseWriter, r *http.Request) {
-	var services []models.Service
-
-	port := utils.GetPort(r)
-	sessionName := "session-" + port
-	curSession, err := session.Store.Get(r, sessionName)
-
-	isAdmin := curSession.Values["isAdmin"].(bool)
-
-	if err := db.Where("is_business = ?", true).Find(&services).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	tmpl, err := template.ParseFiles("templates/business_services/business_services.html",
-		"templates/header/header.html")
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	// Передаем данные в шаблон, включая права доступа
-	err = tmpl.Execute(w, map[string]interface{}{
-		"Services": services,
-		"IsAdmin":  isAdmin, // Передаем информацию о правах доступа
-	})
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func technicalServicesHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Раздел Технические услуги"))
 }
 
 func incidentsHandler(w http.ResponseWriter, r *http.Request) {
