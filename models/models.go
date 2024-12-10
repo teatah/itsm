66b0113be0f97a65bc5c0a0753e88d1d7a56792b
stealthebat <right.seven@yandex.ru>
2024-12-10 22:21:13 +0300

    Add foreign keys

--- models/models.go
+++ models/models.go
@@ -12,35 +12,42 @@ type User struct {
 }
 
 type Service struct {
-	ID          uint   `gorm:"primaryKey"`
-	Name        string `gorm:"not null"`
+	ID          uint   `gorm:"primaryKey" json:"id"`
+	Name        string `gorm:"not null" json:"name"`
 	Description string `json:"description"`
-	IsBusiness  bool   `gorm:"default:false"`
-	IsTechnical bool   `gorm:"default:false"`
+	IsBusiness  bool   `gorm:"default:false" json:"is_business"`
+	IsTechnical bool   `gorm:"default:false" json:"is_technical"`
 }
 
 type Incident struct {
-	ID                uint   `gorm:"primaryKey"`
-	UserID            uint   `gorm:"not null"`
-	ResponsibleUserID uint   `gorm:"not null"`
-	Title             string `gorm:"not null"`
-	Description       string
-	Status            string
-	CreatedAt         time.Time `gorm:"autoCreateTime"`
-	UpdatedAt         time.Time `gorm:"autoUpdateTime"`
+	ID                uint      `gorm:"primaryKey" json:"id"`
+	UserID            uint      `gorm:"not null" json:"user_id"`
+	ResponsibleUserID *uint     `gorm:"default:null" json:"responsible_user_id"`
+	Title             string    `gorm:"not null" json:"title"`
+	Description       string    `json:"description"`
+	Status            string    `json:"status"`
+	CreatedAt         time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt         time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	User              User      `gorm:"foreignKey:UserID" json:"user"`
+	ResponsibleUser   User      `gorm:"foreignKey:ResponsibleUserID" json:"responsible_user"`
 }
 
 type Message struct {
 	ID         uint      `json:"id" gorm:"primaryKey"`
-	DialogID   uint      `json:"dialog_id" gorm:"not null;foreignKey:DialogID"`
-	SenderID   uint      `json:"sender_id" gorm:"not null;foreignKey:UserID"`
-	ReceiverID uint      `json:"receiver_id" gorm:"not null;foreignKey:UserID"`
+	DialogID   uint      `json:"dialog_id" gorm:"not null"`
+	SenderID   uint      `json:"sender_id" gorm:"not null"`
+	ReceiverID uint      `json:"receiver_id" gorm:"not null"`
 	Content    string    `json:"content" gorm:"type:text;not null"`
 	Timestamp  time.Time `json:"timestamp" gorm:"timestamp"`
+	Dialog     Dialog    `gorm:"foreignKey:DialogID" json:"dialog"`
+	Sender     User      `gorm:"foreignKey:SenderID" json:"sender"`
+	Receiver   User      `gorm:"foreignKey:ReceiverID" json:"receiver"`
 }
 
 type Dialog struct {
-	ID            uint `json:"id"`
-	User1ID       uint `json:"user1_id" gorm:"foreignKey:UserID"`
-	User2ID       uint `json:"user2_id" gorm:"foreignKey:UserID"`
+	ID      uint `json:"id" gorm:"primaryKey"`
+	User1ID uint `json:"user1_id" gorm:"not null"`
+	User2ID uint `json:"user2_id" gorm:"not null"`
+	User1   User `gorm:"foreignKey:User1ID" json:"user1"`
+	User2   User `gorm:"foreignKey:User2ID" json:"user2"`
 }
